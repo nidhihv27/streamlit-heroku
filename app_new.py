a59a881234db46eac7fdac047558b1bb9a5c95ef
Nidhi <nidhihv27@gmail.com>
2024-04-21 16:14:27 +0530

    Update app_new.py

--- app_new.py
+++ app_new.py
@@ -8,9 +8,9 @@ st.write("""
 
 st.header('User Input Parameters')
 
-num1 = st.number_input("Number 1")
-num2 = st.number_input("Number 2")
-num3 = st.number_input("Number 3")
+num1 = st.number_input("Number 1",min_value=0,max_value=200000,step=1)
+num2 = st.number_input("Number 2",min_value=0,max_value=200000,step=1)
+num3 = st.number_input("Number 3",min_value=0,max_value=200000,step=1)
 
 st.subheader('The largest Number is')
 if (num1>num2 and num1>num3):
