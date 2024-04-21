28638da3c36767ca68217fe4049164df5854b8be
Nidhi <nidhihv27@gmail.com>
2024-04-21 15:20:40 +0530

    Created app_new.py

---
+++ app_new.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+st.write("""
+# Largest number App
+
+""")
+#Get Input
+
+st.header('User Input Parameters')
+
+num1 = st.number_input("Number 1",min_value=0.0,max_value=2000000.0)
+num2 = st.number_input("Number 2",min_value=0.0,max_value=2000000.0)
+num2 = st.number_input("Number 3",min_value=0.0,max_value=2000000.0)
+
+largestNum = num1
+if (largestNum < num2):
+  largestNum = num2
+if (largestNum < num3):
+  largestNum = num3
+
+
+st.subheader('The largest Number is')
+st.write(largestNum)
