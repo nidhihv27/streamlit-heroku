ed75e4e7e8adebc6b0e1eaf52e0b2ece90d4acb9
Nidhi <nidhihv27@gmail.com>
2024-04-21 16:04:08 +0530

    Update app_new.py

--- app_new.py
+++ app_new.py
@@ -20,4 +20,9 @@ if (largestNum < num3):
 
 
 st.subheader('The largest Number is')
-st.write(largestNum)
+if (num1>num2 and num1>num3)
+  st.write(num1)
+if (num2>num1 and num2>num3)
+  st.write(num2)
+if (num3>num2 and num3>num1)
+  st.write(num3)
